e6a5c90ac1dee9d396ca271ed6f1ba226d0cdaa9
Alex Hoar <thealexhoar@gmail.com>
2017-04-29 10:04:25 -0700

    Re-fixed the untracked file issue.

--- target/rls/debug/deps/radium_core-9eea663542d0232f.d
+++
@@ -1,5 +0,0 @@
-/home/alex/Projects/radium/target/rls/debug/deps/radium_core-9eea663542d0232f.rmeta: src/main.rs
-
-/home/alex/Projects/radium/target/rls/debug/deps/radium_core-9eea663542d0232f.d: src/main.rs
-
-src/main.rs:
