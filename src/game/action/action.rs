1a154a37fa6b0380ef92ea4ebd8c4ccdb4bd5390
Alex Hoar <thealexhoar@gmail.com>
2017-06-05 21:29:24 -0700

    Minor changes.

--- src/game/action/action.rs
+++ src/game/action/action.rs
@@ -1,12 +1,11 @@
 use ecs::*;
 
 pub trait Action {
+    //return whether or not it is completed, and game time elapsed at each step
     fn execute(
         &mut self,
         component_manager: &mut ComponentManager,
         space: &mut Space,
         delta_time: f32
     ) -> (bool, u32);
-    
-    fn reset(&mut self);
 }
